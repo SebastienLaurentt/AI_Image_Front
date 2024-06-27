fcc8639843857566b8f18d0ea0b2572e5a8a7c92
SebastienLaurentt <slaurent.26@gmail.com>
2024-06-27 15:09:30 +0200

    create front

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import FileSaver from 'file-saver';
+import { surpriseMePrompts } from '../constant';
+
+
+export function getRandomPrompt(prompt: string): string {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
+
+export async function downloadImage(_id: string, photo: string): Promise<void> {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
